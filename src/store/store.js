f98fe7b8e4d26e0f3c6b5252385d1d3212820e42
romanytadros <romanytadros@gmail.com>
2022-08-27 11:12:18 +0200

    no

--- src/store/store.js
+++
@@ -1,6 +0,0 @@
-import reducer from './reducer';
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-const store = createStore(reducer, composeWithDevTools());
-
-export default store;
